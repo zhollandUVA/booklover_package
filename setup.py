d789c98928a73e2382b255652887a723625dae73
Zach <nms9dg@virginia.edu>
2024-03-31 19:04:34 -0400

    HW 09 for DS5100

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Booklover',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    
+# Metadata     
+    author='Zachary Holland',
+    author_email='nms9dg@virginia.edu',
+    description='Package for HW09.',
+    keywords='books booklover',
+)
